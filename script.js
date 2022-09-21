b8cf4264e0b3eabf0368e21aa442733ed26c049b
Amy Lipscomb <amylipscomb@amys-mbp.myfiosgateway.com>
2022-09-21 08:53:46 -0400

    Fixed the bug and removed the error message by updating javascript's code, by defining the function, by taking out the parentheses and adding quotes around generatePassword since it's a string

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = "generatePassword"; //The code that was originally here was var password = generatePassword() <-- however this was creating an error message because there should not have been parentheses at the end, and also because it's a string, it should have been in quotes. So, in order to make it work, I took out the parentheses, and put quotes around "generatePassword". When I refreshed the index.html, used the DIV tools to go to the console, the error message was gone. 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
